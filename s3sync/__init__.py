0eab4f3c15c8167bcc1270249921269dc20dc506
Paul Craciunoiu <paul@craciunoiu.net>
2012-10-04 09:44:02 -0700

    Fix get_version() to avoid KeyError

--- s3sync/__init__.py
+++ s3sync/__init__.py
@@ -3,6 +3,6 @@ VERSION = (0, 1)
 
 def get_version():
     version = '%s.%s' % (VERSION[0], VERSION[1])
-    if VERSION[2]:
+    if len(VERSION) > 2:
         version = '%s.%s' % (version, VERSION[2])
     return version
